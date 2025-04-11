dd17ef5f4c7e8cc2cc6fc1d8dc35991b15ec1b7d
Yaswanthkumar939092 <yaswanthkumar9390@gmail.com>
2025-04-11 14:35:51 +0530

    feat: Add after_save event handler to Sales Invoice for recalculating custom amounts in payment schedule

--- arbes_customizations/public/js/sales_invoice.js
+++ arbes_customizations/public/js/sales_invoice.js
@@ -10,5 +10,14 @@ frappe.ui.form.on('Sales Invoice', {
         if (frm.doc.custom_domestic_export === 'Export' && frm.doc.gst_category !== 'Overseas') {
             frappe.throw(__('GST Category must be "Overseas" when Domestic Export is "Export".'));
         }
-    }
+    },
+    after_save: function(frm) {
+        if(frm.doc.payment_schedule){
+            let total = frm.doc.total;
+            frm.doc.payment_schedule.forEach(function(row){
+                row.custom_amount_without_gst = total * (row.invoice_portion / 100);
+            });
+            frm.refresh_field('payment_schedule');
+        }
+    },
 });
